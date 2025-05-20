bbde70cf5995259d544997cc585031b5777c0a32
Jian Shen <jshen@yds-pharmatech.com>
2025-05-20 10:55:09 +0800

    update code examples

--- examples/predict_structure_fks.py
+++ examples/predict_structure_fks.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from fksfold.chai_fks import run_inference
 
+logging.basicConfig(level=logging.INFO)  # control verbosity
+
 # We use fasta-like format for inputs.
 # - each entity encodes protein, ligand, RNA or DNA
 # - each entity is labeled with unique name;
@@ -13,30 +15,35 @@ from fksfold.chai_fks import run_inference
 
 tmp_dir = Path(tempfile.mkdtemp())
 
-fasta_name = "8cyo"
-example_fasta = """
->protein|8cyo-protein
-MKKGHHHHHHGAISLISALVRAHVDSNPAMTSLDYSRFQANPDYQMSGDDTQHIQQFYDLLTGSMEIIRGWAEKIPGFADLPKADQDLLFESAFLELFVLRLAYRSNPVEGKLIFCNGVVLHRLQCVRGFGEWIDSIVEFSSNLQNMNIDISAFSCIAALAMVTERHGLKEPKRVEELQNKIVNTLKDHVTFNNGGLNRPNYLSKLLGKLPELRTLCTQGLQRIFYLKLEDLVPPPAIIDKLFLDTLPF
->ligand|8cyo-ligand
-c1cc(c(cc1OCC(=O)NCCS)Cl)Cl
+fasta_name = "example"
+fasta_context = """
+>protein|name=example-of-long-protein
+AGSHSMRYFSTSVSRPGRGEPRFIAVGYVDDTQFVRFDSDAASPRGEPRAPWVEQEGPEYWDRETQKYKRQAQTDRVSLRNLRGYYNQSEAGSHTLQWMFGCDLGPDGRLLRGYDQSAYDGKDYIALNEDLRSWTAADTAAQITQRKWEAAREAEQRRAYLEGTCVEWLRRYLENGKETLQRAEHPKTHVTHHPVSDHEATLRCWALGFYPAEITLTWQWDGEDQTQDTELVETRPAGDGTFQKWAAVVVPSGEEQRYTCHVQHEGLPEPLTLRWEP
+>protein|name=example-of-short-protein
+AIQRTPKIQVYSRHPAENGKSNFLNCYVSGFHPSDIEVDLLKNGERIEKVEHSDLSFSKDWSFYLLYYTEFTPTEKDEYACRVNHVTLSQPKIVKWDRDM
+>protein|name=example-peptide
+GAAL
+>ligand|name=example-ligand-as-smiles
+CCCCCCCCCCCCCC(=O)O
 """.strip()
 fasta_path = tmp_dir / "example.fasta"
-fasta_path.write_text(example_fasta)
+fasta_path.write_text(fasta_context)
 
 
+# FKS version: Score=0.8915
 output_dir = tmp_dir / "outputs"
 # if you want to use ft steering:
 candidates = run_inference(
     fasta_file=fasta_path,
     output_dir=output_dir,
     # constraint_path="./path_to_contact.restraints",
-    num_trunk_recycles=3,
-    num_diffn_timesteps=200,
+    num_trunk_recycles=1,
+    num_diffn_timesteps=50,
     num_particles=3,         # number of diffusion paths
     resampling_interval=10,  # diffusion path length
     lambda_weight=10.0,      # lower this to, say 2.0, to make it more random
-    potential_type="diff",   # "diff" or "max" or "vanilla"
-    fk_sigma_threshold=float("inf"),
+    potential_type="max",   # "diff" or "max" or "vanilla"
+    # fk_sigma_threshold=float("inf"),
     num_trunk_samples=1,
     seed=42,
     device="cuda:0",
